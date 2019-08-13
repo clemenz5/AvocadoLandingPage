76ee5127ad717daa028010f629d2e061f605d709
Friedrich Schmierer <f.schmierer@getflip.de>
2019-08-13 18:38:43 +0200

    preliminary feature list

--- src/App.svelte
+++ src/App.svelte
@@ -23,4 +23,26 @@
 <section class="hero">
 	<h1>avoca.to - do all the avocado things</h1>
 </section>
+<section class="hero">
+	<h1>Avocado growing in sync with lunar support</h1>
+	<p>Sync your agricultural activity with the phases of the moon and plan your avocado's growth. Retroactively incorporate existing plants into the sky rythm.</p>
+</section>
+<section class="hero">
+	<h1>avoca.to reminds you to be there for your plants.</h1>
+	<p>receive push notifications reminding you to take care of your avocado growing operation. Enable and disable different reminders, like watering, to fit your life.</p>
+</section>
+<section class="hero">
+	<h1>long term planning</h1>
+	<p>Track the time and care you invest into your avocado and receive useful tips. </p>
+</section>
+<section class="hero">
+	<h1>Acoustic Growth Stimulation</h1>
+	<p>connect your avocados to other avoca.to users around the world and let them talk to each other. Stream your fav piece by Mozart straight to them green goodies.</p>
+</section>
+<section>
+<h1>
+	Multicavo.
+</h1>
+<p>Effortlessly manage large-scale (like 5 plant) operations even if they were planted at different times.</p>
+</section>
 
