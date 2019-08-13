2ba9d950a4d71a2a21d7383f2064e658ae1d4de3
clemenz5 <44141973+clemenz5@users.noreply.github.com>
2019-08-13 20:10:58 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -15,31 +15,49 @@
 	section {
 		min-height: 100vh;
 	}
-	.hero {
+	.head {
+		background:  linear-gradient(0deg, #dfffdb, #ffffff);
+	}
+	.feature_moon_calendar {
+		background:  linear-gradient(0deg, #000000, #ffffff);
+	}
+	.feature_notifications {
+		background:  linear-gradient(0deg, #000000, #ffffff);
+	}
+	.feature_planning {
+		background:  linear-gradient(0deg, #000000, #ffffff);
+	}
+	.feature_planning {
+		background:  linear-gradient(0deg, #000000, #ffffff);
+	}
+	.feature_acoustic_growth {
+		background:  linear-gradient(0deg, #000000, #ffffff);
+	}
+	.feature_multicavo {
 		background:  linear-gradient(0deg, #000000, #ffffff);
 	}
 </style>
 
-<section class="hero">
+<section class="head">
 	<h1>avoca.to - do all the avocado things!</h1>
 </section>
-<section class="hero">
+<section class="feature_moon_calendar">
 	<h1>Avocado growing in sync with lunar support</h1>
 	<p>Sync your agricultural activity with the phases of the moon and plan your avocado's growth. Retroactively incorporate existing plants into the sky rythm.</p>
 </section>
-<section class="hero">
+<section class="feature_notifications">
 	<h1>avoca.to reminds you to be there for your plants.</h1>
 	<p>receive push notifications reminding you to take care of your avocado growing operation. Enable and disable different reminders, like watering, to fit your life.</p>
 </section>
-<section class="hero">
+<section class="feature_planning">
 	<h1>long term planning</h1>
 	<p>Track the time and care you invest into your avocado and receive useful tips. </p>
 </section>
-<section class="hero">
+<section class="feature_acoustic_growth">
 	<h1>Acoustic Growth Stimulation</h1>
 	<p>connect your avocados to other avoca.to users around the world and let them talk to each other. Stream your fav piece by Mozart straight to them green goodies.</p>
 </section>
-<section>
+<section class="feature_multicavo">
 <h1>
 	Multicavo.
 </h1>
