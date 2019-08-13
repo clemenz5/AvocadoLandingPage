f89a7d9534a7fa33cae188d23d1f3df9c9f41ad8
Friedrich Schmierer <f.schmierer@getflip.de>
2019-08-13 12:45:25 +0200

    bootstrap svelte app

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let name;
+</script>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+	h1 {
+		color: purple;
+		margin: 0;
+		text-align: center;
+	}
+	section {
+		min-height: 100vh;
+	}
+	.hero {
+		background:  linear-gradient(-164deg, #b7f5b7, #a0ff62);
+	}
+</style>
+
+<section class="hero">
+	<h1>avoca.to - do all the avocado things</h1>
+</section>
+
