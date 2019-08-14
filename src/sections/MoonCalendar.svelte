819e437a186b5ed2fdb0918335b7541f11bc0860
Friedrich Schmierer <f.schmierer@getflip.de>
2019-08-14 11:09:20 +0200

    actually committing :thumbsup:

---
+++ src/sections/MoonCalendar.svelte
@@ -0,0 +1,69 @@
+<style>
+  section {
+    color: lightblue;
+    background: linear-gradient(-28deg, #4125a3, #8691e1);
+    padding-top: 350px;
+
+  }
+  h2 {
+    color: #f1f1ba;
+  }
+  p {
+    max-width: 80%;
+    margin: auto;
+  }
+  svg {
+    position: absolute;
+    top: 3em;
+    right: 1em;
+  }
+</style>
+
+<section>
+  <svg
+    width="440"
+    height="299"
+    viewBox="0 0 440 299"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M395 202.5C395 255.795 351.795 299 298.5 299C245.205 299 202 255.795
+      202 202.5C202 149.205 245.205 106 298.5 106C351.795 106 395 149.205 395
+      202.5Z"
+      fill="#FFEE95" />
+    <path
+      d="M252.67 4.47883L258.385 11.5371L266.864 8.28303L261.917 15.8993L267.632
+      22.9576L258.86 20.6064L253.913 28.2227L253.438 19.1534L244.666
+      16.8022L253.145 13.5482L252.67 4.47883Z"
+      fill="#F3E09C" />
+    <path
+      d="M82.5 190L85.3064 198.637H94.3882L87.0409 203.975L89.8473 212.613L82.5
+      207.275L75.1527 212.613L77.9591 203.975L70.6118 198.637H79.6936L82.5 190Z"
+      fill="#F3E09C" />
+    <path
+      d="M142.18 95.5136L138.352 103.749L145.002 109.935L135.986 108.839L132.158
+      117.075L130.414 108.162L121.399 107.066L129.336 102.654L127.592
+      93.7409L134.242 99.9264L142.18 95.5136Z"
+      fill="#F3E09C" />
+    <path
+      d="M88.5 7L91.3064 15.6373H100.388L93.0409 20.9754L95.8473 29.6127L88.5
+      24.2746L81.1527 29.6127L83.9591 20.9754L76.6118 15.6373H85.6936L88.5 7Z"
+      fill="#F3E09C" />
+    <path
+      d="M427.5 62L430.306 70.6373H439.388L432.041 75.9754L434.847 84.6127L427.5
+      79.2746L420.153 84.6127L422.959 75.9754L415.612 70.6373H424.694L427.5 62Z"
+      fill="#F3E09C" />
+    <path
+      d="M25.8677 94.7162L19.9078 101.569L24.5832 109.355L16.2243
+      105.804L10.2644 112.657L11.0582 103.609L2.69931 100.059L11.5489
+      98.0181L12.3427 88.9711L17.0182 96.7569L25.8677 94.7162Z"
+      fill="#F3E09C" />
+  </svg>
+  <h2>Avocado growing in sync with lunar support</h2>
+
+  <p>
+    Sync your agricultural activity with the phases of the moon and plan your
+    avocado's growth. Retroactively incorporate existing plants into the sky
+    rythm. This also provides us with an excuse to finally learn SVG and CSS animations, so that's cool.
+  </p>
+</section>
