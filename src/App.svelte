4282832a190d5a72e6807bf1bffc1ed4fe12746a
Friedrich Schmierer <f.schmierer@getflip.de>
2019-08-14 11:05:55 +0200

    add moon calendar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,25 @@
 <script>
-	export let name;
+	import MoonCalendar from './sections/MoonCalendar.svelte';
 </script>
 
 <style>
 	:global(html, body) {
 		margin: 0;
 		padding: 0;
+		font-size: 18px;
 	}
-	h1 {
-		color: purple;
-		margin: 0;
+	:global(h1, h2) {
+		font-size: 2em;
+		margin: 0 0 1.5em 0;
 		text-align: center;
 	}
-	section {
+	:global(section) {
 		min-height: 100vh;
+		padding: 2em;
+		position: relative;
 	}
 	.head {
 		background:  linear-gradient(0deg, #dfffdb, #ffffff);
-	}
-	.feature_moon_calendar {
-		background:  linear-gradient(0deg, #b9ffb0, #dfffdb);
 	}
 	.feature_notifications {
 		background:  linear-gradient(0deg, #8eff80, #b9ffb0);
@@ -36,28 +36,25 @@
 </style>
 
 <section class="head">
-	<h1>avoca.to - do all the avocado things!</h1>
+	<h2>avoca.to - do all the avocado things!</h2>
 </section>
-<section class="feature_moon_calendar">
-	<h1>Avocado growing in sync with lunar support</h1>
-	<p>Sync your agricultural activity with the phases of the moon and plan your avocado's growth. Retroactively incorporate existing plants into the sky rythm.</p>
-</section>
+<MoonCalendar />
 <section class="feature_notifications">
-	<h1>avoca.to reminds you to be there for your plants.</h1>
+	<h2>avoca.to reminds you to be there for your plants.</h2>
 	<p>receive push notifications reminding you to take care of your avocado growing operation. Enable and disable different reminders, like watering, to fit your life.</p>
 </section>
 <section class="feature_planning">
-	<h1>long term planning</h1>
+	<h2>long term planning</h2>
 	<p>Track the time and care you invest into your avocado and receive useful tips. </p>
 </section>
 <section class="feature_acoustic_growth">
-	<h1>Acoustic Growth Stimulation</h1>
+	<h2>Acoustic Growth Stimulation</h2>
 	<p>connect your avocados to other avoca.to users around the world and let them talk to each other. Stream your fav piece by Mozart straight to them green goodies.</p>
 </section>
 <section class="feature_multicavo">
-<h1>
+<h2>
 	Multicavo.
-</h1>
+</h2>
 <p>Effortlessly manage large-scale (like 5 plant) operations even if they were planted at different times.</p>
 </section>
 
